79054ee74bb102729fbdc92ed756c4647932eb12
Henrique <fernandes4626@gmail.com>
2025-08-08 23:38:34 -0300

    init: criação dos projetos iniciais

---
+++ MindConnect-Back/src/main/java/br/com/app/hpg/MindConnect/MindConnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.hpg.MindConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindConnectApplication.class, args);
+	}
+
+}
---
+++ MindConnect-Back/src/test/java/br/com/app/hpg/MindConnect/MindConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.hpg.MindConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
